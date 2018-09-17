916e5d68089587ab9017ff31b7d7d8c34a78516f
imjh11 <imjh12@naver.com>
2018-09-18 03:02:16 +0900

    debug

--- cameratest.py
+++ cameratest.py
@@ -6,8 +6,10 @@ import movedetect
 with picamera.PiCamera() as camera:
     while True:
         movedetect.detectmove(True) #will end when move is detected
+        print ("start")
         camera.start_recording('/home/pi/Desktop/video.h264')
         time.sleep(1)
         movedetect.detectmove(False)
+        print("stop")
         camera.stop_recording() #will end when move is not detected
     
