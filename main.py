3cfd92975bce86b1a4ebc83109d3caa18dc0aacf
imjh11 <imjh12@naver.com>
2018-11-04 08:24:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-#import movedetect as mvd
+import movedetect as mvd
 import time
+
 from datetime import datetime
 
 now=str(datetime.now())
 now=now[0:19]
-print(now)
-today=datetime.today()
-print(today)
+with picamera.PiCamera() as camera:
+    while True:
+        mvd.detectmove(True) #will end when move is detected
+        print ("start")
+        start=time.time()
+        starttime=datetime.now().strftime("%y-%m-%d %H-%M-%S")
+        filename=starttime + '.h264'
+        filepath='/home/pi/record/' + filename
+        camera.start_recording(filepath)
+        time.sleep(1)
+        mvd.detectmove(True)
+        print("stop")
+        camera.stop_recording() #will end when move is not detected
+        endtime=datetime.now().strftime("%y-%m-%d %H-%M-%S")
+        end=time.time()
+        length=str(start-end)
+        logoutput=starttime + ' ' + endtime + ' ' + length + '\n'
+        log=open('/home/pi/record/log.txt','a')
+        log.write(logoutput)
+        log.close()
+
+
