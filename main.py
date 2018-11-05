fc8363c6730c6f7df8dcea8cd0c6d6cf8640aacd
imjh11 <imjh12@naver.com>
2018-11-06 07:05:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,19 @@ with picamera.PiCamera() as camera:
         start=time.time()
         starttime=datetime.now().strftime("%y-%m-%d %H-%M-%S")
         filename=starttime + '.h264'
-        filepath='/home/pi/record/' + filename
+        filepath='/home/pi/record/' + filename 
         camera.start_recording(filepath)
         time.sleep(1)
         mvd.detectmove(True)
         print("stop")
-        camera.stop_recording() #will end when move is not detected
+        camera.stop_recording() #will end when move is detected again
         endtime=datetime.now().strftime("%y-%m-%d %H-%M-%S")
         end=time.time()
         length=int(end-start)
-        if length > 120 or length < 10:
+        if length > 120 or length < 10: #warn too short or too long usage
             logoutput=starttime + ' ' + endtime + ' ' + str(length) + ' seconds ' + 'Warning!'+ '\n' + '\n'
         else:
             logoutput=starttime + ' ' + endtime + ' ' + str(length) + ' seconds' + '\n' + '\n'    
         log=open('/home/pi/record/log.txt','a')
         log.write(logoutput)
         log.close()
-
-
